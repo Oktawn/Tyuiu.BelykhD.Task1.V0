7990eb444ab817604fd20a8ddb39b50714b66aa2
oktawn <belyhmitia@gmail.com>
2024-04-03 00:42:25 +0500

    laod csv

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.dataGridView1_Table = new System.Windows.Forms.DataGridView();
+            this.Column_car = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_Phone = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_Master = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_Service = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_Price = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column_date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1_Table)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1_Table
+            // 
+            this.dataGridView1_Table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1_Table.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column_car,
+            this.Column_Phone,
+            this.Column_Master,
+            this.Column_Service,
+            this.Column_Price,
+            this.Column_date});
+            this.dataGridView1_Table.Location = new System.Drawing.Point(12, 160);
+            this.dataGridView1_Table.Name = "dataGridView1_Table";
+            this.dataGridView1_Table.Size = new System.Drawing.Size(646, 278);
+            this.dataGridView1_Table.TabIndex = 0;
+            // 
+            // Column_car
+            // 
+            this.Column_car.HeaderText = "№ car";
+            this.Column_car.MaxInputLength = 9;
+            this.Column_car.Name = "Column_car";
+            this.Column_car.ToolTipText = "number car";
+            // 
+            // Column_Phone
+            // 
+            this.Column_Phone.HeaderText = "Phone";
+            this.Column_Phone.MaxInputLength = 12;
+            this.Column_Phone.Name = "Column_Phone";
+            this.Column_Phone.ToolTipText = "Owner\'s phone number";
+            // 
+            // Column_Master
+            // 
+            this.Column_Master.HeaderText = "Master";
+            this.Column_Master.MaxInputLength = 40;
+            this.Column_Master.Name = "Column_Master";
+            this.Column_Master.ToolTipText = "FIO Master";
+            // 
+            // Column_Service
+            // 
+            this.Column_Service.HeaderText = "Service";
+            this.Column_Service.Name = "Column_Service";
+            // 
+            // Column_Price
+            // 
+            this.Column_Price.HeaderText = "Price";
+            this.Column_Price.Name = "Column_Price";
+            this.Column_Price.ToolTipText = "Cost service";
+            // 
+            // Column_date
+            // 
+            this.Column_date.HeaderText = "Date Complite";
+            this.Column_date.Name = "Column_date";
+            this.Column_date.ToolTipText = "When the job is done";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1_Table);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1_Table)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1_Table;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_car;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_Phone;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_Master;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_Service;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_Price;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column_date;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,20 +1,59 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Tyuiu.BelykhD.Task1.V0
 {
     public partial class Form1 : Form
     {
+        readonly string pathDB = "DB.csv";
         public Form1()
         {
             InitializeComponent();
+            LoadCSV(pathDB);
+        }
+
+
+        private void LoadCSV(string pathDB)
+        {
+            foreach (var line in File.ReadLines(pathDB).Skip(1))
+            {
+                dataGridView1_Table.Rows.Add(line.Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries));
+            }
+
+        }
+        private void SaveCSV()
+        {
+            StringBuilder csv = new StringBuilder();
+
+            for (int i = 0; i < dataGridView1_Table.Columns.Count; i++)
+            {
+                csv.Append(dataGridView1_Table.Columns[i].HeaderText + ";");
+            }
+            csv.Append("\n");
+
+            for (int i = 0; i < dataGridView1_Table.Rows.Count - 1; i++)
+            {
+                for (int j = 0; j < dataGridView1_Table.Columns.Count; j++)
+                {
+                    csv.Append(dataGridView1_Table.Rows[i].Cells[j].Value + ";");
+                }
+                csv.Append("\n");
+            }
+
+            File.WriteAllText(pathDB, csv.ToString());
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Хотите сохранить данные перед закрытием?", "Сохранение данных", MessageBoxButtons.YesNo);
+            if (result == DialogResult.Yes)
+            {
+                SaveCSV();
+            }
         }
     }
 }
